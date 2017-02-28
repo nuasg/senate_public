16d4821176f6c90bb3fb96db13e66b7b438b4ef2
Philip Lan <philiplan2017@u.northwestern.edu>
2017-02-28 13:46:58 -0600

    adding angular

---
+++ app/app.js
@@ -0,0 +1,12 @@
+(function(){
+    angular.module("senate",["ui.router","angular.filter"])
+        .config(function($stateProvider, $urlRouterProvider){
+            $urlRouterProvider.otherwise("/");
+            $stateProvider
+                .state("home", {
+                    url: "/",
+                    templateUrl: "senate_public/app/home/home.html",
+                    controller: "HomeController"
+                });
+        })
+}());
---
+++ app/home/home.controller.js
@@ -0,0 +1,24 @@
+(function(){
+    angular.module("senate")
+    .controller("HomeController",["$scope", "$state", "$http", function($scope, $state, $http){
+        // $http.get("/senate/api/whoami").success(function(user) {
+        //     if (user.role === "Admin") {
+        //         $state.go("admin");
+        //     } else {
+        //         $state.go("senate");
+        //     }
+        // });
+        // $scope.submit = function(){
+        //     if ($scope.role === "Senator") {
+        //         $state.go("senate");
+        //     } else {
+        //         $state.go("admin");
+        //     }
+        // }
+        // $scope.logout = function () {
+        //     $http.post("/senate/logout").success(function(){
+        //         window.location.reload(true);
+        //     });
+        // }
+    }]);
+}());
