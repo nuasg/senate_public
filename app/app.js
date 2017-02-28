a1d5432f04bd2bbef00bce0cb9021930eaf0240f
Philip Lan <philiplan2017@u.northwestern.edu>
2017-02-28 15:59:03 -0600

    changed view structure

--- app/app.js
+++ app/app.js
@@ -5,8 +5,18 @@
             $stateProvider
                 .state("home", {
                     url: "/",
-                    templateUrl: "senate_public/app/home/home.html",
-                    controller: "HomeController"
+                    views: {
+                        "": {
+                            templateUrl: "senate_public/app/home/home.html",
+                            controller: "HomeController"
+                        },
+                        "titlePage@home": {
+                            templateUrl: "senate/app/senate/titlePage.html"
+                        },
+                        "timeline@home": {
+                            templateUrl: "senate/app/senate/timeline.html"
+                        }
+                    }
                 });
         })
 }());
