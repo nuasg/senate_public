fc6cf514ef87a51b3ab24fe3483cd02823ad5c3e
Philip Lan <philiplan2017@u.northwestern.edu>
2017-03-01 01:46:52 -0600

    added legislation results

--- app/legislation/legislation.controller.js
+++ app/legislation/legislation.controller.js
@@ -1,24 +1,44 @@
 (function(){
     angular.module("senate")
     .controller("LegislationController",["$scope", "$state", "$http", function($scope, $state, $http){
-        $scope.state = true;
-        $http.get("senate/api/user").success(function(data){
-            $scope.senators = data.filter(function(person) {
-                if (person.role == "Senator" && person.active && person.firstName) {
-                    return person;
-                }
-            });
-        });
-        $scope.selectSenator = function (senator) {
-            $http.get("senate/api/attendence/" + senator.firstName + "/" + senator.lastName).success(function(attendances){
-                $http.get("senate/api/legislation/user/" + senator.netid).success(function(vote){
-                    $scope.userInfo = {
-                        senator,
-                        attendances,
-                        vote
-                    };
+        var getDocs = function () {
+            $http.get("/senate/api/document").success(
+            function(data) {
+                $scope.data = data.map(function(obj){
+                    obj.weekOf = new Date(obj.weekOf);
+                    return obj;
+                });
+                var idToLegislationMap = {}
+                $scope.data.forEach(function(obj){
+                    if (obj.type == "Legislation"){
+                        idToLegislationMap[obj._id] = [obj.title, obj.weekOf];
+                    }
+                });
+                $http.get("/senate/api/legislation").success(function(votes){
+                    var legislationToVoteMap = {};
+                    votes.forEach(function(vote){
+                        var pair = idToLegislationMap[vote.documentId];
+                        if (pair == undefined) {
+                            return;
+                        }
+                        if (legislationToVoteMap[idToLegislationMap[vote.documentId][0]]) {
+                            if (legislationToVoteMap[idToLegislationMap[vote.documentId][0]][vote.vote]) {
+                                legislationToVoteMap[idToLegislationMap[vote.documentId][0]][vote.vote] += 1;
+                            } else {
+                                legislationToVoteMap[idToLegislationMap[vote.documentId][0]][vote.vote] = 1;
+                            }
+                        } else {
+                            var value = idToLegislationMap[vote.documentId][0];
+                            legislationToVoteMap[value] = {};
+                            legislationToVoteMap[value][vote.vote] = 1;
+                            legislationToVoteMap[value].date = idToLegislationMap[vote.documentId][1];
+                        }
+                    });
+                    $scope.legislationToVoteMap = legislationToVoteMap;
                 });
             });
         }
+        getDocs();
+        var pullDocs = window.setInterval(getDocs, 15000);
     }])
 }());
